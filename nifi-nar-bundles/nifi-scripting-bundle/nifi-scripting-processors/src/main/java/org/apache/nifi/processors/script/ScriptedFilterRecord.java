apache/nifi

NIFI-8273 Adding Scripted Record processors

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/main/java/org/apache/nifi/processors/script/ScriptedFilterRecord.java
@@ -1,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.Relationship;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Tags({"record", "filter", "script", "groovy", "jython", "python"})
+@CapabilityDescription(
+    "This processor provides the ability to filter records out from FlowFiles using the user-provided script. " +
+    "Every record will be evaluated by the script which must return with a boolean value. " +
+    "Records with \"true\" result will be routed to the \"matching\" relationship in a batch. " +
+    "Other records will be filtered out."
+)
+@SeeAlso(classNames = {
+    "org.apache.nifi.processors.script.ScriptedTransformRecord",
+    "org.apache.nifi.processors.script.ScriptedValidateRecord",
+    "org.apache.nifi.processors.script.ScriptedPartitionRecord"
+})
+public class ScriptedFilterRecord extends ScriptedRouterProcessor<Boolean> {
+    static final Relationship RELATIONSHIP_MATCHING = new Relationship.Builder()
+            .name("matching")
+            .description(
+                "Matching records of the original FlowFile will be routed to this relationship. " +
+                "If there are no matching records, no FlowFile will be routed here."
+            )
+            .build();
+
+    static final Relationship RELATIONSHIP_ORIGINAL = new Relationship.Builder()
+            .name("original")
+            .description(
+                "After successful procession, the incoming FlowFile will be transferred to this relationship. " +
+                "This happens regardless the number of filtered or remaining records.")
+            .build();
+
+    static final Relationship RELATIONSHIP_FAILED = new Relationship.Builder()
+            .name("failed")
+            .description("In case of any issue during processing the incoming FlowFile, the incoming FlowFile will be routed to this relationship.")
+            .build();
+
+    private static Set<Relationship> RELATIONSHIPS = new HashSet<>();
+
+    static {
+        RELATIONSHIPS.add(RELATIONSHIP_ORIGINAL);
+        RELATIONSHIPS.add(RELATIONSHIP_FAILED);
+        RELATIONSHIPS.add(RELATIONSHIP_MATCHING);
+    }
+
+    public ScriptedFilterRecord() {
+        super(Boolean.class);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return RELATIONSHIPS;
+    }
+
+    @Override
+    protected Relationship getOriginalRelationship() {
+        return RELATIONSHIP_ORIGINAL;
+    }
+
+    @Override
+    protected Relationship getFailedRelationship() {
+        return RELATIONSHIP_FAILED;
+    }
+
+    @Override
+    protected Optional<Relationship> resolveRelationship(final Boolean scriptResult) {
+        return scriptResult ? Optional.of(RELATIONSHIP_MATCHING) : Optional.empty();
+    }
+}

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/main/java/org/apache/nifi/processors/script/ScriptedValidateRecord.java
@@ -1,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.Relationship;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Tags({"record", "validate", "script", "groovy", "jython", "python"})
+@CapabilityDescription(
+        "This processor provides the ability to validate records in FlowFiles using the user-provided script. " +
+        "The script is expected to have a record as incoming argument and return with a boolean value. " +
+        "Based on this result, the processor categorizes the records as \"valid\" or \"invalid\" and routes them to the respected relationship in batch. " +
+        "Additionally the original FlowFile will be routed to the \"original\" relationship or in case of unsuccessful processing, to the \"failed\" relationship."
+)
+@SeeAlso(classNames = {
+        "org.apache.nifi.processors.script.ScriptedTransformRecord",
+        "org.apache.nifi.processors.script.ScriptedFilterRecord",
+        "org.apache.nifi.processors.script.ScriptedPartitionRecord"
+})
+public class ScriptedValidateRecord extends ScriptedRouterProcessor<Boolean> {
+
+    static final Relationship RELATIONSHIP_VALID = new Relationship.Builder()
+            .name("valid")
+            .description(
+                "FlowFile containing the valid records from the incoming FlowFile will be routed to this relationship. " +
+                "If there are no valid records, no FlowFile will be routed to this Relationship.")
+            .build();
+
+    static final Relationship RELATIONSHIP_INVALID = new Relationship.Builder()
+            .name("invalid")
+            .description(
+                "FlowFile containing the invalid records from the incoming FlowFile will be routed to this relationship. " +
+                "If there are no invalid records, no FlowFile will be routed to this Relationship.")
+            .build();
+
+    static final Relationship RELATIONSHIP_ORIGINAL = new Relationship.Builder()
+            .name("original")
+            .description(
+                "After successful procession, the incoming FlowFile will be transferred to this relationship. " +
+                "This happens regardless the FlowFiles might routed to \"valid\" and \"invalid\" relationships.")
+            .build();
+
+    static final Relationship RELATIONSHIP_FAILED = new Relationship.Builder()
+            .name("failed")
+            .description("In case of any issue during processing the incoming flow file, the incoming FlowFile will be routed to this relationship.")
+            .build();
+
+    private static Set<Relationship> RELATIONSHIPS = new HashSet<>();
+
+    static {
+        RELATIONSHIPS.add(RELATIONSHIP_VALID);
+        RELATIONSHIPS.add(RELATIONSHIP_INVALID);
+        RELATIONSHIPS.add(RELATIONSHIP_ORIGINAL);
+        RELATIONSHIPS.add(RELATIONSHIP_FAILED);
+    }
+
+    public ScriptedValidateRecord() {
+        super(Boolean.class);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return RELATIONSHIPS;
+    }
+
+    @Override
+    protected Relationship getOriginalRelationship() {
+        return RELATIONSHIP_ORIGINAL;
+    }
+
+    @Override
+    protected Relationship getFailedRelationship() {
+        return RELATIONSHIP_FAILED;
+    }
+
+    @Override
+    protected Optional<Relationship> resolveRelationship(final Boolean scriptResult) {
+        return Optional.of(scriptResult ? RELATIONSHIP_VALID : RELATIONSHIP_INVALID);
+    }
+}

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/test/java/org/apache/nifi/processors/script/TestScriptedRouterProcessor.java
@@ -1,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.processor.Processor;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.script.ScriptingComponentUtils;
+import org.apache.nifi.serialization.record.MockRecordParser;
+import org.apache.nifi.serialization.record.MockRecordWriter;
+import org.apache.nifi.serialization.record.RecordFieldType;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Assert;
+import org.junit.Before;
+
+import java.util.UUID;
+
+abstract class TestScriptedRouterProcessor {
+    private static final String HEADER = "header§";
+
+    protected TestRunner testRunner;
+    protected MockRecordParser recordReader;
+    protected MockRecordWriter recordWriter;
+    protected String incomingFlowFileContent;
+
+    @Before
+    public void setUp() throws Exception {
+        testRunner = TestRunners.newTestRunner(givenProcessorType());
+
+        testRunner.setProperty(ScriptedTransformRecord.RECORD_READER, "record-reader");
+        testRunner.setProperty(ScriptedTransformRecord.RECORD_WRITER, "record-writer");
+
+        recordReader = new MockRecordParser();
+        recordReader.addSchemaField("first", RecordFieldType.INT);
+        recordReader.addSchemaField("second", RecordFieldType.STRING);
+
+        recordWriter = new MockRecordWriter(HEADER);
+
+        testRunner.addControllerService("record-reader", recordReader);
+        testRunner.addControllerService("record-writer", recordWriter);
+
+        testRunner.setProperty(ScriptingComponentUtils.SCRIPT_BODY, getScript());
+        testRunner.setProperty(ScriptedTransformRecord.LANGUAGE, "Groovy");
+
+        testRunner.enableControllerService(recordReader);
+        testRunner.enableControllerService(recordWriter);
+
+        incomingFlowFileContent = UUID.randomUUID().toString();
+    }
+
+    protected void whenTriggerProcessor() {
+        testRunner.enqueue(incomingFlowFileContent);
+        testRunner.run();
+    }
+
+    protected void thenIncomingFlowFileIsRoutedToOriginal() {
+        testRunner.assertTransferCount(getOriginalRelationship(), 1);
+        testRunner.assertTransferCount(getFailedRelationship(), 0);
+        Assert.assertEquals(incomingFlowFileContent, testRunner.getFlowFilesForRelationship(getOriginalRelationship()).get(0).getContent());
+    }
+
+    protected void thenIncomingFlowFileIsRoutedToFailed() {
+        testRunner.assertTransferCount(getOriginalRelationship(), 0);
+        testRunner.assertTransferCount(getFailedRelationship(), 1);
+        Assert.assertEquals(incomingFlowFileContent, testRunner.getFlowFilesForRelationship(getFailedRelationship()).get(0).getContent());
+    }
+
+    /**
+     * Generates the expected flow file content based on the records. Results the same format as the {@code MockRecordWriter} uses.
+     */
+    protected String givenExpectedFlowFile(final Object[][] records) {
+        final StringBuilder expectedFlowFile = new StringBuilder(HEADER).append('\n');
+
+        for (final Object[] record : records) {
+            for (int i = 0; i < record.length; i++) {
+                expectedFlowFile.append('"').append(record[i].toString()).append('"');
+
+                if (i < record.length -1 ) {
+                    expectedFlowFile.append(',');
+                }
+            }
+
+            expectedFlowFile.append('\n');
+        }
+
+        return expectedFlowFile.toString();
+    }
+
+    protected abstract Class<? extends Processor> givenProcessorType();
+
+    protected abstract String getScript();
+
+    protected abstract Relationship getOriginalRelationship();
+
+    protected abstract Relationship getFailedRelationship();
+}

KavinKarthik-git/637

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/main/java/org/apache/nifi/processors/script/ScriptedFilterRecord.java
@@ -1,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.Relationship;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Tags({"record", "filter", "script", "groovy", "jython", "python"})
+@CapabilityDescription(
+    "This processor provides the ability to filter records out from FlowFiles using the user-provided script. " +
+    "Every record will be evaluated by the script which must return with a boolean value. " +
+    "Records with \"true\" result will be routed to the \"matching\" relationship in a batch. " +
+    "Other records will be filtered out."
+)
+@SeeAlso(classNames = {
+    "org.apache.nifi.processors.script.ScriptedTransformRecord",
+    "org.apache.nifi.processors.script.ScriptedValidateRecord",
+    "org.apache.nifi.processors.script.ScriptedPartitionRecord"
+})
+public class ScriptedFilterRecord extends ScriptedRouterProcessor<Boolean> {
+    static final Relationship RELATIONSHIP_SUCCESS = new Relationship.Builder()
+            .name("success")
+            .description(
+                "Matching records of the original FlowFile will be routed to this relationship. " +
+                "If there are no matching records, no FlowFile will be routed here."
+            )
+            .build();
+
+    static final Relationship RELATIONSHIP_ORIGINAL = new Relationship.Builder()
+            .name("original")
+            .description(
+                "After successful procession, the incoming FlowFile will be transferred to this relationship. " +
+                "This happens regardless the number of filtered or remaining records.")
+            .build();
+
+    static final Relationship RELATIONSHIP_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("In case of any issue during processing the incoming FlowFile, the incoming FlowFile will be routed to this relationship.")
+            .build();
+
+    private static Set<Relationship> RELATIONSHIPS = new HashSet<>();
+
+    static {
+        RELATIONSHIPS.add(RELATIONSHIP_ORIGINAL);
+        RELATIONSHIPS.add(RELATIONSHIP_FAILURE);
+        RELATIONSHIPS.add(RELATIONSHIP_SUCCESS);
+    }
+
+    public ScriptedFilterRecord() {
+        super(Boolean.class);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return RELATIONSHIPS;
+    }
+
+    @Override
+    protected Relationship getOriginalRelationship() {
+        return RELATIONSHIP_ORIGINAL;
+    }
+
+    @Override
+    protected Relationship getFailureRelationship() {
+        return RELATIONSHIP_FAILURE;
+    }
+
+    @Override
+    protected Optional<Relationship> resolveRelationship(final Boolean scriptResult) {
+        return scriptResult ? Optional.of(RELATIONSHIP_SUCCESS) : Optional.empty();
+    }
+}

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/main/java/org/apache/nifi/processors/script/ScriptedValidateRecord.java
@@ -1,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.Relationship;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Tags({"record", "validate", "script", "groovy", "jython", "python"})
+@CapabilityDescription(
+        "This processor provides the ability to validate records in FlowFiles using the user-provided script. " +
+        "The script is expected to have a record as incoming argument and return with a boolean value. " +
+        "Based on this result, the processor categorizes the records as \"valid\" or \"invalid\" and routes them to the respective relationship in batch. " +
+        "Additionally the original FlowFile will be routed to the \"original\" relationship or in case of unsuccessful processing, to the \"failed\" relationship."
+)
+@SeeAlso(classNames = {
+        "org.apache.nifi.processors.script.ScriptedTransformRecord",
+        "org.apache.nifi.processors.script.ScriptedFilterRecord",
+        "org.apache.nifi.processors.script.ScriptedPartitionRecord"
+})
+public class ScriptedValidateRecord extends ScriptedRouterProcessor<Boolean> {
+
+    static final Relationship RELATIONSHIP_VALID = new Relationship.Builder()
+            .name("valid")
+            .description(
+                "FlowFile containing the valid records from the incoming FlowFile will be routed to this relationship. " +
+                "If there are no valid records, no FlowFile will be routed to this Relationship.")
+            .build();
+
+    static final Relationship RELATIONSHIP_INVALID = new Relationship.Builder()
+            .name("invalid")
+            .description(
+                "FlowFile containing the invalid records from the incoming FlowFile will be routed to this relationship. " +
+                "If there are no invalid records, no FlowFile will be routed to this Relationship.")
+            .build();
+
+    static final Relationship RELATIONSHIP_ORIGINAL = new Relationship.Builder()
+            .name("original")
+            .description(
+                "After successful procession, the incoming FlowFile will be transferred to this relationship. " +
+                "This happens regardless the FlowFiles might routed to \"valid\" and \"invalid\" relationships.")
+            .build();
+
+    static final Relationship RELATIONSHIP_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("In case of any issue during processing the incoming flow file, the incoming FlowFile will be routed to this relationship.")
+            .build();
+
+    private static final Set<Relationship> RELATIONSHIPS = new HashSet<>();
+
+    static {
+        RELATIONSHIPS.add(RELATIONSHIP_VALID);
+        RELATIONSHIPS.add(RELATIONSHIP_INVALID);
+        RELATIONSHIPS.add(RELATIONSHIP_ORIGINAL);
+        RELATIONSHIPS.add(RELATIONSHIP_FAILURE);
+    }
+
+    public ScriptedValidateRecord() {
+        super(Boolean.class);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return RELATIONSHIPS;
+    }
+
+    @Override
+    protected Relationship getOriginalRelationship() {
+        return RELATIONSHIP_ORIGINAL;
+    }
+
+    @Override
+    protected Relationship getFailureRelationship() {
+        return RELATIONSHIP_FAILURE;
+    }
+
+    @Override
+    protected Optional<Relationship> resolveRelationship(final Boolean scriptResult) {
+        return Optional.of(scriptResult ? RELATIONSHIP_VALID : RELATIONSHIP_INVALID);
+    }
+}

nifi-nar-bundles/nifi-scripting-bundle/nifi-scripting-processors/src/test/java/org/apache/nifi/processors/script/TestScriptedRouterProcessor.java
@@ -1,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.script;
+
+import org.apache.nifi.processor.Processor;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.script.ScriptingComponentUtils;
+import org.apache.nifi.serialization.record.MockRecordParser;
+import org.apache.nifi.serialization.record.MockRecordWriter;
+import org.apache.nifi.serialization.record.RecordFieldType;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Assert;
+import org.junit.Before;
+
+import java.util.UUID;
+
+abstract class TestScriptedRouterProcessor {
+    private static final String HEADER = "header§";
+
+    protected TestRunner testRunner;
+    protected MockRecordParser recordReader;
+    protected MockRecordWriter recordWriter;
+    protected String incomingFlowFileContent;
+
+    @Before
+    public void setUp() throws Exception {
+        testRunner = TestRunners.newTestRunner(givenProcessorType());
+
+        testRunner.setProperty(ScriptedTransformRecord.RECORD_READER, "record-reader");
+        testRunner.setProperty(ScriptedTransformRecord.RECORD_WRITER, "record-writer");
+
+        recordReader = new MockRecordParser();
+        recordReader.addSchemaField("first", RecordFieldType.INT);
+        recordReader.addSchemaField("second", RecordFieldType.STRING);
+
+        recordWriter = new MockRecordWriter(HEADER);
+
+        testRunner.addControllerService("record-reader", recordReader);
+        testRunner.addControllerService("record-writer", recordWriter);
+
+        if (getScriptBody() != null) {
+            testRunner.removeProperty(ScriptingComponentUtils.SCRIPT_FILE);
+            testRunner.setProperty(ScriptingComponentUtils.SCRIPT_BODY, getScriptBody());
+        } else if (getScriptFile() != null) {
+            testRunner.removeProperty(ScriptingComponentUtils.SCRIPT_BODY);
+            testRunner.setProperty(ScriptingComponentUtils.SCRIPT_FILE, getScriptFile());
+        }
+
+        testRunner.setProperty(ScriptedTransformRecord.LANGUAGE, "Groovy");
+
+        testRunner.enableControllerService(recordReader);
+        testRunner.enableControllerService(recordWriter);
+
+        incomingFlowFileContent = UUID.randomUUID().toString();
+    }
+
+    protected void whenTriggerProcessor() {
+        testRunner.enqueue(incomingFlowFileContent);
+        testRunner.run();
+    }
+
+    protected void thenIncomingFlowFileIsRoutedToOriginal() {
+        testRunner.assertTransferCount(getOriginalRelationship(), 1);
+        testRunner.assertTransferCount(getFailedRelationship(), 0);
+        Assert.assertEquals(incomingFlowFileContent, testRunner.getFlowFilesForRelationship(getOriginalRelationship()).get(0).getContent());
+    }
+
+    protected void thenIncomingFlowFileIsRoutedToFailed() {
+        testRunner.assertTransferCount(getOriginalRelationship(), 0);
+        testRunner.assertTransferCount(getFailedRelationship(), 1);
+        Assert.assertEquals(incomingFlowFileContent, testRunner.getFlowFilesForRelationship(getFailedRelationship()).get(0).getContent());
+    }
+
+    /**
+     * Generates the expected flow file content based on the records. Results the same format as the {@code MockRecordWriter} uses.
+     */
+    protected String givenExpectedFlowFile(final Object[]... records) {
+        final StringBuilder expectedFlowFile = new StringBuilder(HEADER).append('\n');
+
+        for (final Object[] record : records) {
+            for (int i = 0; i < record.length; i++) {
+                expectedFlowFile.append('"').append(record[i].toString()).append('"');
+
+                if (i < record.length -1 ) {
+                    expectedFlowFile.append(',');
+                }
+            }
+
+            expectedFlowFile.append('\n');
+        }
+
+        return expectedFlowFile.toString();
+    }
+
+    protected abstract Class<? extends Processor> givenProcessorType();
+
+    protected abstract Relationship getOriginalRelationship();
+
+    protected abstract Relationship getFailedRelationship();
+
+    protected String getScriptBody() {
+        return null;
+    }
+
+    protected String getScriptFile() {
+        return null;
+    }
+}